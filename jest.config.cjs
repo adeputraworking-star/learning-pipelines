39a0863829aa1ac356ca4fd1435f0ccce8097d13
Ade Putra Susila <adeputra.vendor.pasti@pegadaian.co.id>
2026-01-14 15:33:58 +0700

    FEAT: Added Unit Test Coverage 100%

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,24 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+
+  roots: ["<rootDir>/tests"],
+
+  collectCoverage: true,
   collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts'
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/*.js"
   ],
+
+  coverageDirectory: "coverage",
+
   coverageThreshold: {
     global: {
+      statements: 80,
       branches: 80,
       functions: 80,
-      lines: 80,
-      statements: 80
+      lines: 80
     }
   }
 };
